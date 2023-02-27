b84e05b1e6a76f48745c6588a103bc58d732268f
MasterHermit <rajpruthwi5534.ps@gmail.com>
2023-02-27 15:14:14 +0530

    routing initialised

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/COURSE/Hero";
+import Hero from "./pages/COURSE/Hero";
 function App() {
   return (
     <div className="App">
-      app
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          
+        </Routes>
+      </Router>
     </div>
   );
 }

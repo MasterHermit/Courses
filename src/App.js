6923a46aeb5dda5104ea8e81b13da3ece69cba05
Gourav Samal <gouravsamal100@gmail.com>
2023-02-27 16:09:12 +0530

    nbn

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/COURSE/Hero";
 import Hero from "./pages/COURSE/Hero";
+
 function App() {
   return (
     <div className="App">
+
       <Router>
         <NavBar />
         <Routes>
---
+++ src/Auth.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Auth() {
+  return (
+    <div>
+      This is for 
+      1.Login
+      2.Register
+      3.Forget Password
+      4.OTP Login 
+    </div>
+  )
+}

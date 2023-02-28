1a0efc0323263829733841b214f4455544d81e47
Sriti23 <sritirout@gmail.com>
2023-02-28 16:43:04 +0530

    Card is updated

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from 'react'
+import "./Card.css"
 
-const Cards = () => {
+const Card = () => {
     return (
-        <div>
-            
+  <div className="wrapper">
+  <div className="cards_wrap">
+    <div className="card_item">
+      <div className="card_inner">
+        <div className="card_top">
+          <img src="React.png" alt="card image" />
         </div>
+        <div className="card_bottom">
+          <div className="card_category">
+            React
+          </div>
+          <div className="card_info">
+            <p className="title">
+              The Complete guide to learn advance React
+            </p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              sdog9eqrgsdgkjqdm sadgjjqjrg sp asdgjoiq0gas  adgjdfk;j asdfjlksjdf 
+              sdfkjasld sldkfoasosd
+            </p>
+          </div>
+          <div className="card_creator">
+            By Sriti Rout
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
     );
 };
 
-export default Cards;
+export default Card;

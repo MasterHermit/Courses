b2a3368ac723e27a1d4097c92bee156a54d5d78e
MasterHermit <rajpruthwi5534.ps@gmail.com>
2023-03-14 14:54:19 +0530

    user slice added

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { userReducer, changeLoggedIn } from "./slices/userSlice";
 import { coursesApi } from "./apis/coursesApi";
 import { quizApi } from "./apis/quizApi";
 import { socialApi } from "./apis/socialApi";
 import { feedbackApi } from "./apis/feedbackApis";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
+    user: userReducer,
     [coursesApi.reducerPath]: coursesApi.reducer,
     [quizApi.reducerPath]: quizApi.reducer,
     [socialApi.reducerPath]: socialApi.reducer,
@@ -28,4 +30,4 @@ export { useFetchCoursesQuery, useFetchCourseQuery } from "./apis/coursesApi";
 export { useFetchQuizQuery } from "./apis/quizApi";
 export { useFetchAllPostQuery, useCreatePostMutation, useUpdatePostLikeMutation } from "./apis/socialApi";
 export { useFetchFeedbacksQuery } from "./apis/feedbackApis";
-
+export { store, changeLoggedIn }
